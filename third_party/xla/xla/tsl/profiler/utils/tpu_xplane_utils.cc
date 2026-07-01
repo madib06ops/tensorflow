1976919e32d4df80fcd0021dd59cb8eec35d175d
A. Unique TensorFlower <gardener@tensorflow.org>
2026-06-30 17:21:23 -0700

    This change generalizes the SparseCore regex parsing in XLA profiler utilities to match any hardware subtype as well. It replaces explicit constants with a generic regex `SparseCore(?: [a-zA-Z]+)? ([0-9]+)$`.
    PiperOrigin-RevId: 940740850

--- third_party/xla/xla/tsl/profiler/utils/tpu_xplane_utils.cc
+++ third_party/xla/xla/tsl/profiler/utils/tpu_xplane_utils.cc
@@ -50,11 +50,8 @@ std::optional<int> GetTensorCoreId(absl::string_view plane_name) {
 }
 
 std::optional<int> GetSparseCoreId(absl::string_view plane_name) {
-  std::optional<int> core_id;
+  int core_id = -1;
   if (RE2::FullMatch(plane_name, {kSparseCorePlaneRegex}, &core_id)) {
-    return core_id;
-  }
-  if (RE2::FullMatch(plane_name, {kSparseCoreCAEPlaneRegex}, &core_id)) {
     return core_id;
   }
   return std::nullopt;
